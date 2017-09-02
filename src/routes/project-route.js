25dd4c5c74ba68843280ecb3346b15908eb22e03
Tiago de lima <tiago_lima411@hotmail.com>
2017-09-02 15:54:37 -0300

    controle de rotas via token

--- src/routes/project-route.js
+++ src/routes/project-route.js
@@ -5,11 +5,11 @@ const router     = express.Router();
 const controller = require('../controllers/project-controler');
 const authService= require('../services/auth-service')
 
-router.delete('/:titleProj',controller.delete);
-router.get('/:titleProj',controller.getByTitle);
-router.get('/',controller.get);
-router.post('/',controller.create);
-router.put('/:id',controller.put);
+router.delete('/:titleProj',authService.authorize,controller.delete);
+router.get('/:titleProj',authService.authorize,controller.getByTitle);
+router.get('/',authService.authorize,controller.get);
+router.post('/',authService.authorize,controller.create);
+router.put('/:id',authService.authorize,controller.put);
 
 
 module.exports = router;
